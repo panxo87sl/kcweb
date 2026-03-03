bd08a32312aa441a82288fcd218a696418645366
panxo87sl <savage87.sl@gmail.com>
2026-03-03 19:11:49 -0300

    Mod mensaje wasap se agrega nombre del especialista

--- src/components/OsteoPage/OsteoPage.jsx
+++ src/components/OsteoPage/OsteoPage.jsx
@@ -83,7 +83,7 @@ export default function OsteoPage() {
                       label={"Whatsapp"}
                       href={"https://wa.me/56950833838"}
                       variant="whatsapp"
-                      wspmsg={"Hola, me gustaría agendar una cita con el especialista"}
+                      wspmsg={"Hola, me gustaría agendar una cita con el kinesiologo Diego"}
                       size="nav"
                     />
                   </div>
